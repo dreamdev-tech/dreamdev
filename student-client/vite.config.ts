073dcaf30a12d0e57d8eb19b1835d4e3f78932e1
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-12-25 21:54:46 +0100

    remove analyzer

--- student-client/vite.config.ts
+++ student-client/vite.config.ts
@@ -1,10 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { analyzer } from "vite-bundle-analyzer";
+// import { analyzer } from "vite-bundle-analyzer";
 
 export default defineConfig({
-  plugins: [react(), analyzer()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
